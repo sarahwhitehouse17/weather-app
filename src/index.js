2e370d8e56b32b7b77037144249053df8ab7a371
sarahwhitehouse17 <sarahwhitehouse81@gmail.com>
2024-09-26 16:39:01 +0200

    Added API to search for city and show temp

--- src/index.js
+++ src/index.js
@@ -1,10 +1,26 @@
+function refreshWeather(response) {
+  console.log(response.data.temperature.current);
+  let temperatureElement = document.querySelector("#temperature");
+  let newCity = document.querySelector("#weather-app-city");
+  newCity.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+}
+
+function searchCity(city) {
+  let apiKey = "3deb07c475cdt015b4f399a253e4o0b8";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  console.log(apiUrl);
+  axios.get(apiUrl).then(refreshWeather);
+}
+
 function findCity(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
   console.log(searchInput.value);
-  let newCity = document.querySelector("#weather-app-city");
-  newCity.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", findCity);
+
+searchCity("Madrid");
